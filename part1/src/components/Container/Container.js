b21331cebde08ee772a931360685522a46232923
vilhjalmur <vilhjalmur12@ru.is>
2018-02-13 20:45:48 +0000

    container fix

--- part1/src/components/Container/Container.js
+++ part1/src/components/Container/Container.js
@@ -3,12 +3,9 @@ import ListView from '../ListView/ListView';
 
 const Container = () => {
     return (
-        //<div className="top-margin-container">
             <div className="ui raised very padded text container segment top-margin-container">
                 <ListView />
             </div>
-        //</div>
-
     );
 };
 
